f649c9f298446e37d031a84984105584d9734c7c
Harmen <harmendark@gmail.com>
2019-07-29 23:04:19 +0100

    Added project start

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost/url-shortner";
+const connectOptions = { 
+  keepAlive: true, 
+  reconnectTries: Number.MAX_VALUE 
+}; 
+
+require('./models/UrlShorten')
+
+
+//Connect to MongoDB 
+mongoose.Promise = global.Promise; 
+mongoose.connect(mongoURI, connectOptions, (err, db) => 
+{ 
+  if (err) console.log(`Error`, err); 
+  console.log(`Connected to MongoDB`); 
+}); 
+const app = express(); 
+app.use(bodyParser.json());
+const PORT = 7000; 
+require("./routes/urlshorten")(app);
+app.listen(PORT, () => { 
+  console.log(`Server started on port`, PORT); 
+});
